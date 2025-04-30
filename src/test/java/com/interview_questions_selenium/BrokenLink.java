580c1609f03244c1d1f55fd152c3ca0c66a6444b
Mangesh Panchwagh <mangesh013@gmail.com>
2025-04-30 18:28:22 +0530

    broken link hyr

--- src/test/java/com/interview_questions_selenium/BrokenLink.java
+++ src/test/java/com/interview_questions_selenium/BrokenLink.java
@@ -1,6 +1,8 @@
 package com.interview_questions_selenium;
 
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashSet;
@@ -26,26 +28,35 @@ public class BrokenLink {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		driver.get("https://www.hyrtutorials.com");
+		driver.get("https://www.hyrtutorials.com/");
 		Thread.sleep(2000);
 		
 		Set<String> brokenLinkUrls = new HashSet<String>();
+		
 		List<WebElement> links = driver.findElements(By.tagName("a"));
 		System.out.println(links.size());
 		
 		for (WebElement link : links) {
 			String linkURL = link.getAttribute("href");
+			
+			Proxy proxy = new Proxy(java.net.Proxy.Type.HTTP,new InetSocketAddress("hostname",8080));
 			URL url = new URL(linkURL);
-			URLConnection urlConnection = url.openConnection();
+			URLConnection urlConnection = url.openConnection(proxy);
 			HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
 			httpURLConnection.setConnectTimeout(5000);
 			httpURLConnection.connect();
-		
+//		
+//			if(httpURLConnection.getResponseCode() == 200)
+//				System.out.println(linkURL + " - " + httpURLConnection.getResponseMessage());
+//			else
+//				System.err.println(linkURL + " - " + httpURLConnection.getResponseCode());
+			
 			if(httpURLConnection.getResponseCode() != 200 ) {
 				
 				brokenLinkUrls.add(linkURL);
 			}
 
+		
 			
 			httpURLConnection.disconnect();
 			
